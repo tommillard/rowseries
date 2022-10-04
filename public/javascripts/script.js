861656afaa100c69374e47db24a47650ef06b04f
Tom Millard <tommmillard81@gmail.com>
2022-10-04 20:46:31 +0100

    gitignore

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,36 @@
+import { Grid, html } from "https://unpkg.com/gridjs?module";
+
+let settings = {};
+let grid = new Grid({
+  columns: ["Name", "1A", "1B", "2A", "2B", "Division"],
+  data: [],
+}).render(document.getElementById("wrapper"));
+let data;
+
+setTimeout(() => {
+  // lets update the config
+  grid
+    .updateConfig({
+      data: data.slice(0, 10),
+    })
+    .forceRender();
+}, 10000);
+
 fetch("../json/scrape.json")
   .then(function (response) {
     return response.json();
   })
   .then(function (j) {
-    let now = new Date();
-    let then = new Date(j.scrapedAt);
-
-    let timeDiff = new Date(now - then).toISOString().slice(11, 19);
-
-    console.log(timeDiff);
+    data = j.athletes;
+    drawGrid();
     console.table(j.athletes);
   });
+
+function drawGrid() {
+  grid
+    .updateConfig({
+      columns: ["Name", "1A", "1B", "2A", "2B", "Division"],
+      data: data,
+    })
+    .forceRender();
+}
