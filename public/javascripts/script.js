7327078299fe7f850a4681ba6ebe8f320dcd775c
Tom Millard <tommmillard81@gmail.com>
2022-10-05 07:17:23 +0100

    wrote own grid lib

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,36 +1,38 @@
-import { Grid, html } from "https://unpkg.com/gridjs?module";
-
 let settings = {};
-let grid = new Grid({
-  columns: ["Name", "1A", "1B", "2A", "2B", "Division"],
-  data: [],
-}).render(document.getElementById("wrapper"));
-let data;
+let rawData;
+let presentationData;
+let wrapper = document.getElementById("wrapper");
 
-setTimeout(() => {
-  // lets update the config
-  grid
-    .updateConfig({
-      data: data.slice(0, 10),
-    })
-    .forceRender();
-}, 10000);
+function rsElem(type, appendTo, classNames, innerHTML) {
+  let elem = document.createElement(type);
+  if (classNames) {
+    elem.setAttribute("class", classNames);
+  }
+  if (innerHTML) {
+    elem.textContent = innerHTML;
+  }
+  if (appendTo) {
+    appendTo.appendChild(elem);
+  }
+}
 
 fetch("../json/scrape.json")
   .then(function (response) {
     return response.json();
   })
   .then(function (j) {
-    data = j.athletes;
+    rawData = j.athletes;
     drawGrid();
     console.table(j.athletes);
   });
 
 function drawGrid() {
-  grid
-    .updateConfig({
-      columns: ["Name", "1A", "1B", "2A", "2B", "Division"],
-      data: data,
-    })
-    .forceRender();
+  let presentationData = formatData(rawData);
+  for (let athlete of presentationData) {
+    rsElem("div", wrapper, "row", athlete.name);
+  }
+}
+
+function formatData(raw) {
+  return raw;
 }
