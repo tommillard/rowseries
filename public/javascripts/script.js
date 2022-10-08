70bb9253ebebed1e196efc0c044c5eeb6d9e8a0e
Tom Millard <tommillard@users.noreply.github.com>
2022-10-08 14:04:00 +0100

    remove Sven

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -258,9 +258,6 @@ function drawGrid() {
 
 function addDivisions(raw) {
     var divisionedData = raw.map(function (athlete) {
-        if (athlete.name.split(" ")[0] == "Sven") {
-            alert(athlete.name);
-        }
         athlete.category = divisions.find(
             (division) => athlete.category === division.title
         );
