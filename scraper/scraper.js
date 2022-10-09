8b1e8cadf2e67b04c8ef54d44e54b19d34dbab2c
Tom Millard <tommillard@users.noreply.github.com>
2022-10-09 21:21:20 +0100

    removed comments

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,12 +1,4 @@
-/* var express = require("express");
-var router = express.Router();
 
-router.get("/", function (req, res, next) {
-  console.log("hello");
-  res.render("index", { title: "Express" });
-});
-
-module.exports = router; */
 
 const puppeteer = require("puppeteer");
 fs = require("fs-extra");
